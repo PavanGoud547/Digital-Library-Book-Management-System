4fa6dfc52f45fd1955ffd086ccbb628aaf291a7d
Pavan Goud <pavankumarp547@gmail.com>
2025-03-26 17:42:48 +0530

    "Implemented REST API endpoints"

---
+++ src/main/java/Pavan/in/Digital/Library/Book/Management/System/controller/BookController.java
@@ -0,0 +1,49 @@
+package Pavan.in.Digital.Library.Book.Management.System.controller;
+
+import Pavan.in.Digital.Library.Book.Management.System.model.Book;
+import Pavan.in.Digital.Library.Book.Management.System.service.BookService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+@CrossOrigin(origins = "http://localhost:3000") // Allow React frontend
+public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+        return ResponseEntity.ok(bookService.getBookById(id));
+    }
+
+    @GetMapping("/search")
+    public List<Book> searchBooks(@RequestParam String title) {
+        return bookService.searchBooksByTitle(title);
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> addBook(@Valid @RequestBody Book book) {
+        return ResponseEntity.ok(bookService.addBook(book));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id, @Valid @RequestBody Book book) {
+        return ResponseEntity.ok(bookService.updateBook(id, book));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
+        return ResponseEntity.noContent().build();
+    }
+}
