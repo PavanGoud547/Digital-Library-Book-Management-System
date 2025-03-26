1097002f1d5f2ba97eab6975658cc3e048c7d78b
Pavan Goud <pavankumarp547@gmail.com>
2025-03-26 17:31:58 +0530

    "Added Book entity, AvailabilityStatus enum and repository"

---
+++ src/main/java/Pavan/in/Digital/Library/Book/Management/System/model/AvailabilityStatus.java
@@ -0,0 +1,5 @@
+package Pavan.in.Digital.Library.Book.Management.System.model;
+public enum AvailabilityStatus {
+    AVAILABLE,
+    CHECKED_OUT
+}
---
+++ src/main/java/Pavan/in/Digital/Library/Book/Management/System/model/Book.java
@@ -0,0 +1,34 @@
+package Pavan.in.Digital.Library.Book.Management.System.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
+
+@Entity
+@Table(name = "books")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    @NotBlank(message = "Author is required")
+    private String author;
+
+    @NotBlank(message = "Genre is required")
+    private String genre;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private AvailabilityStatus status;
+}
